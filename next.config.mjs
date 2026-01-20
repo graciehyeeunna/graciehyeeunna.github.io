2036f7f35ae7fae9ac123700d3793ef46cb9877c
chaerinKim <chae-rin.kim@pwc.com>
2026-01-20 17:13:38 +0900

    fix : 스타일 빌드 관련 오류 해결

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // 루트 도메인(https://graciehyeeunna.github.io)을 사용하므로 basePath가 필요 없습니다.
   basePath: "",
   assetPrefix: "",
   trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  webpack: (config) => {
-    return config;
   },
 };
 
