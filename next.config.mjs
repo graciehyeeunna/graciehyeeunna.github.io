f425778e76fce98137454a5e91d3f4504987241e
chaerinKim <chae-rin.kim@pwc.com>
2026-01-20 16:04:15 +0900

    fix :  코드이전 후 에러 해결

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/gracieNa",
-  assetPrefix: "/gracieNa/",
+  // basePath: "/graciehyeeunna",
+  // assetPrefix: "/graciehyeeunna/",
   trailingSlash: true, // 정적 배포 시 라우팅 문제 해결을 위해 추가
   images: {
     unoptimized: true,
